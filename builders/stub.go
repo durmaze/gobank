e421eb26e5b533c422fe4254c7e3bde23b7c1517
alperkose <alperkose@gmail.com>
2016-01-24 16:11:00 +0200

    adds predicates to the Stub and StubBuilder

--- builders/stub.go
+++ builders/stub.go
@@ -1,27 +1,38 @@
 package builders
 
-type Stub struct{
-	Responses []Response `json:"responses"`
+type Stub struct {
+	Responses  []Response  `json:"responses"`
+	Predicates []Predicate `json:"predicates"`
 }
 
-type stubBuilder struct{
-	responses []Response
+type stubBuilder struct {
+	responses  []Response
+	predicates []Predicate
 }
 
 type StubBuilder interface {
 	AddResponse(Response) StubBuilder
+	AddPredicate(Predicate) StubBuilder
+
 	Build() Stub
 }
 
 func (builder *stubBuilder) AddResponse(response Response) StubBuilder {
-	builder.responses = []Response{ response }
+	builder.responses = append(builder.responses, response)
+
+	return builder
+}
+
+func (builder *stubBuilder) AddPredicate(predicate Predicate) StubBuilder {
+	builder.predicates = append(builder.predicates, predicate)
 
 	return builder
 }
 
 func (builder *stubBuilder) Build() Stub {
 	return Stub{
-		Responses: builder.responses,
+		Responses:  builder.responses,
+		Predicates: builder.predicates,
 	}
 }
 
--- builders/stub_test.go
+++ builders/stub_test.go
@@ -13,20 +13,20 @@ var _ = Describe("Stub Builder Tests", func() {
 
 	Describe("When building a Stub with single Response", func() {
 		var (
-			actualResponse Response
+			actualResponse   Response
 			expectedResponse Response
-			once sync.Once
+			once             sync.Once
 		)
 
 		BeforeEach(func() {
-			once.Do(func(){
+			once.Do(func() {
 
 				expectedResponse = Response{
 					Is: Is{
 						StatusCode: 200,
-						Headers: map[string]string {
-							"Content-Type" : "application/json",
-							"X-Custom-Header" : "ABC123",
+						Headers: map[string]string{
+							"Content-Type":    "application/json",
+							"X-Custom-Header": "ABC123",
 						},
 						Body: "{ \"greeting\": \"Hello GoBank\" }",
 					},
@@ -36,7 +36,7 @@ var _ = Describe("Stub Builder Tests", func() {
 
 				actualResponse = stub.Responses[0]
 			})
-	  })
+		})
 
 		It("should create a Stub that returns a Response with the correct StatusCode", func() {
 			Expect(actualResponse.Is.StatusCode).To(Equal(expectedResponse.Is.StatusCode))
@@ -49,10 +49,74 @@ var _ = Describe("Stub Builder Tests", func() {
 		It("should create a Stub that returns a Response with the correct Custom header", func() {
 			Expect(actualResponse.Is.Headers["X-Custom-Header"]).To(Equal(expectedResponse.Is.Headers["X-Custom-Header"]))
 		})
-		
+
 		It("should create a Stub that returns a Response with the correct Body", func() {
 			Expect(actualResponse.Is.Body).To(Equal(expectedResponse.Is.Body))
 		})
+	})
+
+	Describe("When building a Stub with single Response and a single Equals predicate", func() {
+		var (
+			actualResponse   Response
+			expectedResponse Response
+
+			actualPredicate   Predicate
+			expectedPredicate Predicate
+
+			once sync.Once
+		)
+
+		BeforeEach(func() {
+			once.Do(func() {
+
+				expectedResponse = Response{
+					Is: Is{
+						StatusCode: 200,
+						Headers: map[string]string{
+							"Content-Type":    "application/json",
+							"X-Custom-Header": "ABC123",
+						},
+						Body: "{ \"greeting\": \"Hello GoBank\" }",
+					},
+				}
+
+				expectedPredicate = Predicate{
+					Equals: Equals{
+						Path: "/test-path",
+					},
+				}
+
+				stub := NewStubBuilder().AddResponse(expectedResponse).AddPredicate(expectedPredicate).Build()
+
+				actualResponse = stub.Responses[0]
+				actualPredicate = stub.Predicates[0]
+			})
+		})
+
+		It("should create a Stub that returns a Response with the correct StatusCode", func() {
+			Expect(actualResponse.Is.StatusCode).To(Equal(expectedResponse.Is.StatusCode))
+		})
+
+		It("should create a Stub that returns a Response with the correct Content-Type header", func() {
+			Expect(actualResponse.Is.Headers["Content-Type"]).To(Equal(expectedResponse.Is.Headers["Content-Type"]))
+		})
+
+		It("should create a Stub that returns a Response with the correct Custom header", func() {
+			Expect(actualResponse.Is.Headers["X-Custom-Header"]).To(Equal(expectedResponse.Is.Headers["X-Custom-Header"]))
+		})
+
+		It("should create a Stub that returns a Response with the correct Body", func() {
+			Expect(actualResponse.Is.Body).To(Equal(expectedResponse.Is.Body))
+		})
+
+		It("should create a Stub that has a Predicate with type \"Equals\"", func() {
+			Expect(actualPredicate.Equals).NotTo(BeNil())
+		})
+
+		It("should create a Stub that has a Predicate with correct Path condition", func() {
+			Expect(actualPredicate.Equals.Path).To(Equal(expectedPredicate.Equals.Path))
+		})
+
 	})
 
 })
