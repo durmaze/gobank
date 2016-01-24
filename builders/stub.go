a41e1481c9b86f559e0433ca4f1cc1c02dbeb866
Erkan Durmaz <edurmaz@thoughtworks.com>
2016-01-24 02:21:55 +0200

    adds TODO.txt, adds Response headers support

--- builders/stub.go
+++ builders/stub.go
@@ -10,6 +10,7 @@ type Response struct{
 
 type Is struct{
 	StatusCode int `json:"statusCode"`
+	Headers map[string]string `json:"headers"`
 	Body string `json:"body"`
 }
 
--- builders/stub_test.go
+++ builders/stub_test.go
@@ -20,8 +20,13 @@ var _ = Describe("Stub Builder Tests", func() {
 
 		BeforeEach(func() {
 			once.Do(func(){
+				headers := make(map[string]string)
+				headers["Content-Type"] = "application/json"
+				headers["X-Custom-Header"] = "ABC123"
+
 				is := Is{
 					StatusCode: 200,
+					Headers: headers,
 					Body: "{ \"greeting\": \"Hello GoBank\" }",
 				}
 
@@ -39,6 +44,14 @@ var _ = Describe("Stub Builder Tests", func() {
 			Expect(actualResponse.Is.StatusCode).To(Equal(expectedResponse.Is.StatusCode))
 		})
 
+		It("should create a Stub that returns a Response with the correct Content-Type header", func() {
+			Expect(actualResponse.Is.Headers["Content-Type"]).To(Equal(expectedResponse.Is.Headers["Content-Type"]))
+		})
+
+		It("should create a Stub that returns a Response with the correct Custom header", func() {
+			Expect(actualResponse.Is.Headers["X-Custom-Header"]).To(Equal(expectedResponse.Is.Headers["X-Custom-Header"]))
+		})
+		
 		It("should create a Stub that returns a Response with the correct Body", func() {
 			Expect(actualResponse.Is.Body).To(Equal(expectedResponse.Is.Body))
 		})
