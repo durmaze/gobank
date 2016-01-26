8652cf1ce5f25a2887bbc107d08385f39ce12dbf
Erkan Durmaz <edurmaz@thoughtworks.com>
2016-01-27 00:06:39 +0200

    adds test case for multiple predicates scenario

--- builders/stub_test.go
+++ builders/stub_test.go
@@ -95,4 +95,71 @@ var _ = Describe("Stub Builder Tests", func() {
 
 	})
 
+	Describe("When building a Stub with single Response and multiple Equals predicates", func() {
+		var (
+			actualResponse   Response
+			expectedResponse Response
+
+			actualPredicate1    Predicate
+			actualPredicate2    Predicate			
+			expectedPredicate1 Predicate
+			expectedPredicate2 Predicate
+
+			once sync.Once
+		)
+
+		BeforeEach(func() {
+			once.Do(func() {
+
+				expectedResponse = Response{
+					Is: Is{
+						StatusCode: 200,
+						Body:       "{ \"greeting\": \"Hello GoBank\" }",
+					},
+				}
+
+				expectedPredicate1 = Predicate{
+					Equals: Equals{
+						Path: "/test-path",
+					},
+				}
+
+
+				expectedPredicate2 = Predicate{
+					Equals: Equals{
+						Method: "POST",
+					},
+				}
+
+				stub := NewStubBuilder().AddResponse(expectedResponse).AddPredicate(expectedPredicate1).AddPredicate(expectedPredicate2).Build()
+
+				actualResponse = stub.Responses[0]
+				actualPredicate1 = stub.Predicates[0]
+				actualPredicate2 = stub.Predicates[1]
+			})
+		})
+
+		It("should create a Stub that returns a Response with the correct StatusCode", func() {
+			Expect(actualResponse.Is.StatusCode).To(Equal(expectedResponse.Is.StatusCode))
+		})
+
+		It("should create a Stub that returns a Response with the correct Body", func() {
+			Expect(actualResponse.Is.Body).To(Equal(expectedResponse.Is.Body))
+		})
+
+		It("should create a Stub that has 2 Predicates of type \"Equals\"", func() {
+			Expect(actualPredicate1.Equals).NotTo(BeNil())
+			Expect(actualPredicate2.Equals).NotTo(BeNil())			
+		})
+
+		It("should create a Stub that has a Predicate with correct Path condition", func() {
+			Expect(actualPredicate1.Equals.Path).To(Equal(expectedPredicate1.Equals.Path))
+		})
+
+		It("should create a Stub that has a Predicate with correct Method condition", func() {
+			Expect(actualPredicate2.Equals.Method).To(Equal(expectedPredicate2.Equals.Method))
+		})
+
+	})
+
 })
